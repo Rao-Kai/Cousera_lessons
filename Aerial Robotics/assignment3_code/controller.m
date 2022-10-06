8e9fc3f6fdecae8b577f97c457c72b4d24620e06
Kai Rao <55841295+Rao-Kai@users.noreply.github.com>
2022-10-06 15:28:38 +0800

    Add files via upload

---
+++ Aerial Robotics/assignment3_code/controller.m
@@ -0,0 +1,56 @@
+function [F, M] = controller(t, state, des_state, params)
+%CONTROLLER  Controller for the quadrotor
+%
+%   state: The current state of the robot with the following fields:
+%   state.pos = [x; y; z], state.vel = [x_dot; y_dot; z_dot],
+%   state.rot = [phi; theta; psi], state.omega = [p; q; r]
+%
+%   des_state: The desired states are:
+%   des_state.pos = [x; y; z], des_state.vel = [x_dot; y_dot; z_dot],
+%   des_state.acc = [x_ddot; y_ddot; z_ddot], des_state.yaw,
+%   des_state.yawdot
+%
+%   params: robot parameters
+
+%   Using these current and desired states, you have to compute the desired
+%   controls
+kpx = 180;
+kdx = 35;
+kpz = 90;
+kdz = 20;
+kpphi = 100;
+kdphi = 1.8;
+
+kpy = kpx;
+kdy = kdx;
+kppsi = kpphi;
+kdpsi = kdphi;
+kptheta = kpphi;
+kdtheta = kdphi;
+
+% =================== Your code goes here ===================
+
+% 根据11式计算
+r1_des_ddot = des_state.acc(1) + kdx*(des_state.vel(1)-state.vel(1)) + kpx*(des_state.pos(1)-state.pos(1));
+r2_des_ddot = des_state.acc(2) + kdy*(des_state.vel(2)-state.vel(2)) + kpy*(des_state.pos(2)-state.pos(2));
+r3_des_ddot = des_state.acc(3) + kdz*(des_state.vel(3)-state.vel(3)) + kpz*(des_state.pos(3)-state.pos(3));
+
+% 根据14式计算
+phi_des = (1/params.gravity)*(r1_des_ddot*des_state.yaw - r2_des_ddot);
+theta_des = (1/params.gravity)*(r1_des_ddot + r2_des_ddot*des_state.yaw);
+
+
+% Thrust F就是u1 (pdf中的13式)
+% F = 0;
+F = params.mass*(params.gravity + r3_des_ddot);
+
+% Moment M就是u2 (pdf中的10式) p q r是三个角的角速度，用state.omega = [p; q; r]表示
+% 正如pdf第5页说到，控制时通常考虑无人机的nominal hover state, 即roll 和 pitch很小, 那么p和q的desir就设定为0
+% M = zeros(3,1);
+M = [kpphi*(phi_des-state.rot(1)) + kdphi*(0-state.omega(1));
+     kptheta*(theta_des-state.rot(2)) + kdtheta*(0-state.omega(2));
+     kppsi*(des_state.yaw-state.rot(3)) + kdpsi*(des_state.yawdot-state.omega(3))];
+
+% =================== Your code ends here ===================
+
+end
---
+++ Aerial Robotics/assignment3_code/runsim.m
@@ -0,0 +1,35 @@
+close all;
+clear;
+
+
+addpath('utils');
+
+
+%% pre-calculated trajectories
+% trajhandle = @traj_line;
+% trajhandle = @traj_helix;
+
+%% Trajectory generation with waypoints
+%% You need to implement this
+trajhandle = @traj_generator;
+waypoints = [0    0   0;
+             1    1   1;
+             2    0   2;
+             3    -1  1;
+             4    0   0]';
+% waypoints = [0    0   1;
+%              -1    1   1;
+%              0    2   1;
+%              1.4    2  1;
+%              2.4    1   1;
+%              1.4 0 1;
+%              0.5 1 1]';
+trajhandle([],[],waypoints);
+
+%% controller
+controlhandle = @controller;
+
+
+% Run simulation with given trajectory generator and controller
+% state - n x 13, with each row having format [x, y, z, xdot, ydot, zdot, qw, qx, qy, qz, p, q, r]
+[t, state] = simulation_3d(trajhandle, controlhandle);
